33c4183c6edbd7abe818546fcf7d24f967ae90df
tomstoten <tomstoughton1@gmail.com>
2023-03-07 15:22:35 -0700

    first reconstructor run

--- cobra_test.py
+++ cobra_test.py
@@ -2,6 +2,6 @@ import cobra
 from cobra.io import load_model
 from reconstructor import reconstruct
 
-input_file = 'data/GCF_001645765.1_ASM164576v1_genomic.fna'
-model = reconstruct(input_file, file_type=1)
+input_file = 'data/protein.faa'
+model = reconstruct(input_file, file_type=1, gram='negative', cpu=2)
 print(model)
